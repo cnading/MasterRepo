205728b3ff375fdbc4134b5ec1b48788a168c86c
cole.nading <cole.nading@MU-C1-10.mullen.pvt.k12.co.us>
2020-01-09 11:13:13 -0700

    added monkey

--- Boldness/src/Boldness.java
+++ Boldness/src/Boldness.java
@@ -5,7 +5,8 @@ public class Boldness
 		public static void main(String[] args)
 			{
 				System.out.println("Be Bold");
-				System.out.println("Hellllloooooo");
+				System.out.println("I want a pet monkey!");
 			}
-
+		
+		
 	}
