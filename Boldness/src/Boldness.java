91abc98c6ceadf27e4608834a97facf0b5ace71d
cole.nading <cole.nading@MU-C1-10.mullen.pvt.k12.co.us>
2020-01-09 11:42:34 -0700

    Cleaning up

--- Boldness/src/Boldness.java
+++ Boldness/src/Boldness.java
@@ -4,13 +4,17 @@ public class Boldness
 
 		public static void main(String[] args)
 			{
+				intro();
+			}
+
+		private static void intro()
+			{
 				System.out.println("Be Bold");
 				System.out.println("I want a pet monkey!");
-
 				System.out.println("No Be Bolder");
-
 				System.out.println("Hydrate or Diedrayte!");
-
+				System.out.println("tanners cheese");
+				
 			}
 		
 		
